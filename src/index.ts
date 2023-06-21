c9d91c6e8fbf144cf4193712f48b1eaf1bf86a48
MoiraWalker-Ordina <81765027+MoiraWalker-Ordina@users.noreply.github.com>
2023-06-21 15:42:16 +0200

    p5 content

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,39 @@
 import * as p5 from "p5";
 
 export const sketch = (p: p5) => {
+  let outputScale = 2;
+  let strokeColor = "#6355C0";
+  let strokeW = 0.8;
+  let bgColor = "#292A41";
+
   p.setup = () => {
-    p.createCanvas(400, 400);
+    p.pixelDensity();
+    p.createCanvas(1000 * outputScale, 1000 * outputScale, p.WEBGL);
+    p.background(bgColor);
   };
 
   p.draw = () => {
-    p.background(220);
-    p.ellipse(50, 50, 80, 80);
+    p.scale(outputScale);
+    p.noFill();
+    p.rotateY(p.mouseY * 400);
+    p.rect(100, 100, 100, 100, 5);
+    p.stroke(strokeColor);
+    p.strokeWeight(strokeW);
+
+    p.rotateY(p.mouseY * 400);
+    p.rect(0, -200, 100, 100, 5);
+    p.stroke(strokeColor);
+    p.strokeWeight(strokeW);
+
+    p.rotateY(p.mouseY * 100);
+    p.rect(200, 100, 100, 100, 20);
+    p.stroke(strokeColor);
+    p.strokeWeight(strokeW);
+
+    p.rotateY(p.mouseY * 200);
+    p.rect(200, -200, 100, 100, 10);
+    p.stroke(strokeColor);
+    p.strokeWeight(strokeW);
   };
 };
 
